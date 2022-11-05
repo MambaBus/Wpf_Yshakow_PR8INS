070ba18347a1cdf328e7e6cd38b339c89882eb30
MambaBusinka <ban123fav@gmail.com>
2022-11-05 16:24:49 +1000

    Пример 1

--- Wpf_Yshakow_PR8INS/Window1.xaml.cs
+++ Wpf_Yshakow_PR8INS/Window1.xaml.cs
@@ -23,5 +23,29 @@ namespace Wpf_Yshakow_PR8INS
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Cчитывание начальных данных
+
+            double x0 = Convert.ToDouble(X0.Text);
+            double xk = Convert.ToDouble(Xk.Text);
+            double dx = Convert.ToDouble(Dx.Text);
+            double a = Convert.ToDouble(A.Text);
+
+            //Цикл для табулирования функции
+
+            double x = x0;
+
+            while (x <= (xk + dx / 2))
+            {
+                double y = a * Math.Log(x);
+
+                X.Text += "x=" + Convert.ToString(x) + Environment.NewLine;
+                Y.Text += "y=" + Convert.ToString(y) + Environment.NewLine;
+
+                x = x + dx;
+            }
+        }
     }
 }
