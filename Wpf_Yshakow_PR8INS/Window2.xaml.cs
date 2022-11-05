8bb5cf669097b5dbb02ee2e094f9117adbc2bd6a
MambaBusinka <ban123fav@gmail.com>
2022-11-05 16:40:31 +1000

    Доделано

--- Wpf_Yshakow_PR8INS/Window2.xaml.cs
+++ Wpf_Yshakow_PR8INS/Window2.xaml.cs
@@ -23,5 +23,29 @@ namespace Wpf_Yshakow_PR8INS
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Cчитывание начальных данных
+
+            double x0 = Convert.ToDouble(X0.Text);
+            double xk = Convert.ToDouble(Xk.Text);
+            double dx = Convert.ToDouble(Dx.Text);
+            double b = Convert.ToDouble(B.Text);
+
+            //Цикл для табулирования функции
+
+            double x = x0;
+
+            while (x <= (xk + dx / 2))
+            {
+                double y = 9*(Math.Pow(x, 3) + Math.Pow(b, 3))*Math.Tan(x);
+
+                X.Text += "x=" + Convert.ToString(x) + Environment.NewLine;
+                Y.Text += "y=" + Convert.ToString(y) + Environment.NewLine;
+
+                x = x + dx;
+            }
+        }
     }
 }
